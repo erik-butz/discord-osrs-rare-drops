5f6a4f9b514e55f7d0b3b0ed2847b121b069a62a
Erik Butz <erikbutzlive@gmail.com>
2024-10-03 14:08:46 -0500

    chore: reading messages and getting embeds

--- src/bot/readMessage.js
+++ src/bot/readMessage.js
@@ -5,13 +5,17 @@ const readNewMessages = () => {
   // We use 'c' for the event parameter to keep it separate from the already defined 'client'
   client.on("messageCreate", message => {
     // Only reading messages in specific Channel
-    if (message.channelId = '1289963294710431744') {
+    // 1263911679268487299 = rare drops channel
+    // 1289963294710431744 = raredroptesting channel
+    if (message.channelId = '1263911679268487299') {
       console.log(message);
       // console.log(message.id !== 1290041386879680653);
       // Ignoring Discord bot message?.author?.id
       if (message?.author?.id !== '1289952988525232238') {
         // console.log(JSON.stringify(message));
-        message.channel.send('NEW MESSAGE ALERT');
+
+        // message.channel.send('NEW MESSAGE ALERT');
+
         // message.reply(`Replying to message ${message.content}`);
       }
     }
