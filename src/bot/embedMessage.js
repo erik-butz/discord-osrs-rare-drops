67fa277ed3d9f882786929e6ffabecfc98f55b76
Erik Butz <erikbutzlive@gmail.com>
2024-10-06 21:54:34 -0500

    chore: functioning filtering rsn's and items and posting embeded messages

--- src/bot/embedMessage.js
+++ src/bot/embedMessage.js
@@ -1,28 +1,25 @@
 const { EmbedBuilder } = require('discord.js');
 
+
 const embedBuilder = (embedsData) => {
   // const owner = embedsData?.author?.name;
   // const descrpition = embedsData?.author?.descrpition;
   const embedMessage = new EmbedBuilder()
-    .setTitle('TESTING');
-  // .setColor(0x0099FF)
-  // .setTitle('')
-  // .setURL('https://discord.js.org/')
-  // .setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
-  // .setDescription('Some description here')
-  // .setThumbnail('https://i.imgur.com/AfFp7pu.png')
-  // .addFields(
-  //   { name: 'Regular field title', value: 'Some value here' },
-  //   { name: '\u200B', value: '\u200B' },
-  //   { name: 'Inline field title', value: 'Some value here', inline: true },
-  //   { name: 'Inline field title', value: 'Some value here', inline: true },
-  // )
-  // .addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
-  // .setImage('https://i.imgur.com/AfFp7pu.png')
-  // .setTimestamp()
-  // .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
-  // channel.send({ embeds: [embedMessage] });
+    .setTitle(embedsData?.author?.name)
+    .setColor(0x0099FF)
+    .setURL('https://discord.js.org/')
+    // .setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+    .setDescription(embedsData?.description)
+    .setThumbnail(embedsData?.thumbnail?.url)
+    .addFields(
+      { name: 'GE Value', value: embedsData?.fields[2]?.value },
+    )
+    // .addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+    // .setTimestamp()
+    .setFooter({ text: 'Butz OSRS Rare Drops Bot', iconURL: 'https://static.wikia.nocookie.net/darkrunescape/images/a/ae/Default.PNG/revision/latest?cb=20071210112833' });
   return embedMessage;
 };
+
+
 
 module.exports = { embedBuilder };
--- src/bot/readMessage.js
+++ src/bot/readMessage.js
@@ -1,23 +1,40 @@
 const { client } = require('./botConnection');
 const { embedBuilder } = require('./embedMessage');
+const rsnArray = require('../../whitelistRSNs.json');
+const itemArray = require('../../whitelistItems.json');
 
 const readNewMessages = () => {
   // When the client is ready, run this code (only once)
   // We use 'c' for the event parameter to keep it separate from the already defined 'client'
   client.on("messageCreate", message => {
     // Only reading messages in specific Channel
+    const embedsData = message.embeds[0].data;
     // 1263911679268487299 = rare drops channel
     // 1289963294710431744 = raredroptesting channel
-    if (message.channelId = '1263911679268487299') {
-      const embedsData = message.embeds[0].data;
-      console.log(message.embeds[0].data);
+    if (message.channelId == '1263911679268487299') {
       // Ignoring Discord bot message?.author?.id
       if (message?.author?.id !== '1289952988525232238') {
-        const messageBuilt = embedBuilder(embedsData);
-        message.channel.send({ embeds: [messageBuilt] });
+        const rsnFiltered = rsnFilterHelper(embedsData?.author?.name);
+        const itemFiltered = itemFilterHelper(embedsData?.description);
+        console.log(itemFiltered);
+        if (rsnFiltered.length !== 0 && itemFiltered.length !== 0) {
+          const messageBuilt = embedBuilder(embedsData);
+          message.channel.send({ embeds: [messageBuilt] });
+        }
       }
     }
   });
+};
+
+const rsnFilterHelper = (runeScapeName) => {
+  return nameFound = rsnArray?.names.filter((rsn) => rsn.toLowerCase() === runeScapeName.toLowerCase());
+};
+
+const itemFilterHelper = (description) => {
+  const regex = /(?<=\[).+?(?=\])/;
+  const foundItem = description.match(regex);
+  console.log(foundItem[0].toLowerCase());
+  return itemFound = itemArray?.names.filter((item) => item.toLowerCase() === foundItem[0].toLowerCase());
 };
 
 module.exports = { readNewMessages };
