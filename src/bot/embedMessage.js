d219bf71c841bc81fe6142ac7fb9bb8bdd0dd790
Erik Butz <erikbutzlive@gmail.com>
2024-11-23 14:12:49 -0600

    chore: pushing up recent changes

--- src/bot/embedMessage.js
+++ src/bot/embedMessage.js
@@ -2,25 +2,30 @@ const { EmbedBuilder } = require('discord.js');
 
 
 const embedBuilder = (embedsData) => {
-  const embedMessage = new EmbedBuilder();
-  embedMessage.setTitle(embedsData?.author?.name);
-  embedMessage.setColor(0x0099FF);
-  embedMessage.setDescription(embedsData?.description);
-  embedMessage.setFooter({
-    text: 'CAL-ZONE OSRS Rare Drops Bot', iconURL: 'https://static.wikia.nocookie.net/darkrunescape/images/a/ae/Default.PNG/revision/latest?cb=20071210112833'
-  });
-  // Only add the description GE Value field for non pet items (Not present on object in pet event)
-  if (!embedsData?.description.includes('pet')) {
-    embedMessage.addFields({ name: 'GE Value', value: embedsData?.fields[2]?.value });
-    embedMessage.setThumbnail(embedsData?.thumbnail?.url);
-  }
+  try {
+    const embedMessage = new EmbedBuilder();
+    embedMessage.setTitle(embedsData?.author?.name);
+    embedMessage.setColor(0x0099FF);
+    embedMessage.setDescription(embedsData?.description);
+    embedMessage.setFooter({
+      text: 'CAL-ZONE OSRS Rare Drops Bot', iconURL: 'https://static.wikia.nocookie.net/darkrunescape/images/a/ae/Default.PNG/revision/latest?cb=20071210112833'
+    });
+    // Only add the description GE Value field for non pet items (Not present on object in pet event)
+    if (!embedsData?.description.includes('pet')) {
+      embedMessage.addFields({ name: 'GE Value', value: embedsData?.fields[2]?.value });
+      embedMessage.setThumbnail(embedsData?.thumbnail?.url);
+    }
 
-  // Extra fields that can be used to modify EmbedBuilder class
-  // const owner = embedsData?.author?.name;
-  // const descrpition = embedsData?.author?.descrpition;
-  // .setURL('https://discord.js.org/')
-  // .setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
-  return embedMessage;
+    // Extra fields that can be used to modify EmbedBuilder class
+    // const owner = embedsData?.author?.name;
+    // const descrpition = embedsData?.author?.descrpition;
+    // .setURL('https://discord.js.org/')
+    // .setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+    return embedMessage;
+  } catch (err) {
+    console.log(`Error in embedBuilder: ${err}`);
+    return;
+  }
 };
 
 module.exports = { embedBuilder };
--- src/bot/readMessage.js
+++ src/bot/readMessage.js
@@ -6,27 +6,36 @@ const itemArray = require('../../whitelistItems.json');
 const readNewMessages = () => {
   try {
     client.on("messageCreate", message => {
-      // Only reading messages in specific Channel
-      // console.log(message?.embeds[0]);
-      // 1263911679268487299 = rare drops channel
-      if (message?.channelId == '1263911679268487299' && message?.author?.id !== '1289952988525232238') {
-        const embedsData = message?.embeds[0]?.data;
-        const rsnFiltered = rsnFilterHelper(embedsData?.author?.name);
-        const itemFiltered = itemFilterHelper(embedsData?.description);
-        // Checks to make sure pet isn't in it because the Embed message data for Pet has almost nothing in it.
-        if (embedsData?.description.includes('pet')) {
-          const messageBuilt = embedBuilder(embedsData);
-          client.channels.cache.get(`1293327521168887922`).send({ embeds: [messageBuilt] });
-        }
-        // Checks whitelisted items & userss first
-        // Makes sure the loot isn't from pvp (loot chest)
-        else if (rsnFiltered?.length !== 0 && itemFiltered?.length !== 0 && !embedsData?.description.includes('Loot Chest')) {
-          console.log(message?.embeds[0]);
-          const messageBuilt = embedBuilder(embedsData);
-          client.channels.cache.get(`1293327521168887922`).send({ embeds: [messageBuilt] });
+
+      // Filter out screenshots (comes in as a map and shouldn't be there on normal embeds messages)
+      if (!message?.attachments?.size) {
+        // Only reading messages in specific Channel
+        // 1263911679268487299 = rare drops channel
+        if (message?.channelId == '1263911679268487299' && message?.author?.id !== '1289952988525232238') {
+          const embedsData = message?.embeds[0]?.data;
+          // Logging out empty description field for troubleshooting
+          if (!embedsData?.description) {
+            console.log(`MISSING DESCRIPTION: ${embedsData}`);
+          }
+          const rsnFiltered = rsnFilterHelper(embedsData?.author?.name);
+          const itemFiltered = itemFilterHelper(embedsData?.description);
+          // Checks to make sure pet isn't in it because the Embed message data for Pet has almost nothing in it.
+          if (embedsData?.description.includes('pet.')) {
+            console.log(message?.embeds);
+            const messageBuilt = embedBuilder(embedsData);
+            client.channels.cache.get(`1293327521168887922`).send({ embeds: [messageBuilt] });
+          }
+          // Checks whitelisted items & userss first
+          // Makes sure the loot isn't from pvp (loot chest)
+          else if (rsnFiltered?.length !== 0 && itemFiltered?.length !== 0 && !embedsData?.description.includes('Loot Chest')) {
+            console.log(message?.embeds[0]);
+            const messageBuilt = embedBuilder(embedsData);
+            client.channels.cache.get(`1293327521168887922`).send({ embeds: [messageBuilt] });
+          }
         }
       }
     });
+
   } catch (err) {
     console.log(err?.message ?? err);
   }
@@ -37,11 +46,17 @@ const rsnFilterHelper = (runeScapeName) => {
 };
 
 const itemFilterHelper = (description) => {
-  const regex = /(?<=\[).+?(?=\])/;
-  let foundItem = description?.match(regex);
-  foundItem[0] = foundItem[0].replace(/'/, ''); ///cleaning up apostrophe's
-  // console.log(foundItem[0].toLowerCase());
-  return itemFound = itemArray?.names.filter((item) => item?.toLowerCase() === foundItem[0]?.toLowerCase());
+  try {
+    const regex = /(?<=\[).+?(?=\])/;
+    let foundItem = description?.match(regex);
+    foundItem[0] = foundItem[0]?.replace(/'/, ''); ///cleaning up apostrophe's
+    // console.log(foundItem[0].toLowerCase());
+    return itemFound = itemArray?.names?.filter((item) => item?.toLowerCase() === foundItem[0]?.toLowerCase());
+  }
+  catch (err) {
+    console.error(`Error in itemFilterHelper for: ${description}`);
+    throw new Error();
+  }
 };
 
 module.exports = { readNewMessages };
